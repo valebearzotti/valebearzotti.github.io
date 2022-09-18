5f2d43b2ba62901c99f5d075979e40fcf51ca9eb
Valentina Bearzotti <valebearzotti1@gmail.com>
2022-09-18 10:36:35 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+function Home() {
+    return (
+        <div>Home</div>
+    )
+}
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+`
+
+export default Home
